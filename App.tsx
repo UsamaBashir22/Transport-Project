4df1bd3e40ec10e893213ae9072cdc9fbe2ff109
UsamaBashir22 <152492781+UsamaBashir22@users.noreply.github.com>
2024-08-06 20:43:05 +0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,378 @@
+import React from 'react'
+import "./react.css"
+
+
+
+
+function App() {
+
+  return (
+
+    <div>
+  
+  <div className="big-image">
+  <div className="container">
+
+  <div className="text">
+            <p className="title-line-height"><span className="title-bolder">Lorem ipsum dolar sit amet consectetur </span> <br/>
+                Find <span className="title-bolder">Local bus timing</span>
+                between <span className="title-bolder">cities</span> in <span className="title-bolder">Senegal</span></p>
+        </div>
+
+    </div>
+   
+<div className="box-parent-1">
+
+
+<div className="parent-advance-1">
+    {/* <i className="fa-regular fa-flag flag-advance-1"></i> */}
+    <select name="" id=""  className="drop-icon-items drop-icon-advance-1">
+        
+        <option value="" className="wo">Dakar Z1</option>      
+        </select>
+    <img src="dropdown.png" alt="" className="parent-advance-icon"/>
+</div>
+
+<div className="parent-advance-2">
+    {/* <i className="fa-solid fa-location-dot  location-advance-2"></i> */}
+    <select name="" id=""  className="drop-icon-items drop-icon-advance-2">
+        <option value="">Dakar Z2</option>
+    </select>
+    <img src="dropdown.png" alt="" className="parent-advance-icon"/>
+</div>
+
+<div className="parent-advance-3">
+    {/* <i className="fa-sharp fa-solid fa-calendar-days calendar-advance-3"></i> */}
+    <input name="" id="" type="date" className="drop-icon-items drop-icon-advance-3"/>
+</div>
+
+<div>
+<button className="button-advance">Search</button>
+</div>
+
+<div >
+    <img src="arrow.png" alt="" className="advance-image"/>
+</div>
+</div>
+</div>
+
+
+<div className="backgroud-box-6-7">
+<div className="box-parent-3">
+
+    <div className="urban">
+        <p><b>Urban</b></p>
+    </div>
+
+    <div className="intercity">
+        <p>Intercity</p>
+    </div>
+</div>
+
+
+<div className="father-box">
+
+
+<div className="box-parent-6">
+
+    
+ <div className="box-parent-66">
+
+<div className="box-child-6-1">
+
+<div className="box-child-6-1-1">
+    <button className="box-button-child-6-1-1-1">urban</button>
+</div>
+<div className="box-child-6-1-2">
+    <b className="box-child-6-1-2-1">Lebanon Line1</b>
+</div>
+
+</div>
+
+<div className="box-child-6-2">
+    <div className=" box-child-6-2-1">
+        {/* <i className="fa-regular fa-flag box-child-6-2-1-1"></i>  */}
+        <b className="box-child-6-2-1-2">10:00</b>
+        <p className="box-child-6-2-1-3">Ideatolifel</p>
+    </div>
+
+    <div className="box-child-6-2-2">
+        <p className="box-child-6-2-2-1">10 m</p>
+         <p className="box-child-6-2-2-2"> (1 stop)</p>
+    </div>
+
+    <div className="box-child-6-2-3">
+        {/* <i className="fa-solid fa-location-dot box-child-6-2-3-1"></i> */}
+        <b className="box-child-6-2-3-2">10:00</b>
+        <p className="box-child-6-2-3-3">Mochrou3 cafe</p>
+    </div>
+</div>
+
+    </div>
+
+<div className="box-child-6-3">
+    <button className="box-child-button-6-3-1">View Deatails</button>
+</div>
+
+</div>
+</div>
+
+
+<div className="father-box">
+<div className="box-parent-6">
+
+    
+    <div className="box-parent-66">
+   
+   <div className="box-child-6-1">
+   
+   <div className="box-child-6-1-1">
+       <button className="box-button-child-6-1-1-1">urban</button>
+   </div>
+   <div className="box-child-6-1-2">
+       <b className="box-child-6-1-2-1">Lebanon Line1</b>
+   </div>
+   
+   </div>
+   
+   <div className="box-child-6-2">
+       <div className=" box-child-6-2-1">
+           {/* <i className="fa-regular fa-flag box-child-6-2-1-1"></i>  */}
+           <b className="box-child-6-2-1-2">10:00</b>
+           <p className="box-child-6-2-1-3">Ideatolifel</p>
+       </div>
+   
+       <div className="box-child-6-2-2">
+           <p className="box-child-6-2-2-1">10 m</p>
+            <p className="box-child-6-2-2-2"> (1 stop)</p>
+       </div>
+   
+       <div className="box-child-6-2-3">
+           {/* <i className="fa-solid fa-location-dot box-child-6-2-3-1"></i> */}
+           <b className="box-child-6-2-3-2">10:00</b>
+           <p className="box-child-6-2-3-3">Mochrou3 cafe</p>
+       </div>
+   </div>
+   
+       </div>
+   
+   <div className="box-child-6-3">
+       <button className="box-child-button-6-3-1">View Deatails</button>
+   </div>
+   
+   </div>
+</div>
+
+
+
+<div className="father-box">
+    <div className="box-parent-6">
+    
+        
+        <div className="box-parent-66">
+       
+       <div className="box-child-6-1">
+       
+       <div className="box-child-6-1-1">
+           <button className="box-button-child-6-1-1-1">urban</button>
+       </div>
+       <div className="box-child-6-1-2">
+           <b className="box-child-6-1-2-1">Lebanon Line1</b>
+       </div>
+       
+       </div>
+       
+       <div className="box-child-6-2">
+           <div className=" box-child-6-2-1">
+               {/* <i className="fa-regular fa-flag box-child-6-2-1-1"></i>  */}
+               <b className="box-child-6-2-1-2">10:00</b>
+               <p className="box-child-6-2-1-3">Ideatolifel</p>
+           </div>
+       
+           <div className="box-child-6-2-2">
+               <p className="box-child-6-2-2-1">10 m</p>
+                <p className="box-child-6-2-2-2"> (1 stop)</p>
+           </div>
+       
+           <div className="box-child-6-2-3">
+               {/* <i className="fa-solid fa-location-dot box-child-6-2-3-1"></i> */}
+               <b className="box-child-6-2-3-2">10:00</b>
+               <p className="box-child-6-2-3-3">Mochrou3 cafe</p>
+           </div>
+       </div>
+       
+           </div>
+       
+       <div className="box-child-6-3">
+           <button className="box-child-button-6-3-1">View Deatails</button>
+       </div>
+       
+       </div>
+    </div>
+
+    
+<div className="father-box">
+    <div className="box-parent-6">
+    
+        
+        <div className="box-parent-66">
+       
+       <div className="box-child-6-1">
+       
+       <div className="box-child-6-1-1">
+           <button className="box-button-child-6-1-1-1">urban</button>
+       </div>
+       <div className="box-child-6-1-2">
+           <b className="box-child-6-1-2-1">Lebanon Line1</b>
+       </div>
+       
+       </div>
+       
+       <div className="box-child-6-2">
+           <div className=" box-child-6-2-1">
+               {/* <i className="fa-regular fa-flag box-child-6-2-1-1"></i>  */}
+               <b className="box-child-6-2-1-2">10:00</b>
+               <p className="box-child-6-2-1-3">Ideatolifel</p>
+           </div>
+       
+           <div className="box-child-6-2-2">
+               <p className="box-child-6-2-2-1">10 m</p>
+                <p className="box-child-6-2-2-2"> (1 stop)</p>
+           </div>
+       
+           <div className="box-child-6-2-3">
+               {/* <i className="fa-solid fa-location-dot box-child-6-2-3-1"></i> */}
+               <b className="box-child-6-2-3-2">10:00</b>
+               <p className="box-child-6-2-3-3">Mochrou3 cafe</p>
+           </div>
+       </div>
+       
+           </div>
+       
+       <div className="box-child-6-3">
+           <button className="box-child-button-6-3-1">View Deatails</button>
+       </div>
+       
+       </div>
+    </div>
+
+    
+<div className="father-box">
+    <div className="box-parent-6">
+    
+        
+        <div className="box-parent-66">
+       
+       <div className="box-child-6-1">
+       
+       <div className="box-child-6-1-1">
+           <button className="box-button-child-6-1-1-1">urban</button>
+       </div>
+       <div className="box-child-6-1-2">
+           <b className="box-child-6-1-2-1">Lebanon Line1</b>
+       </div>
+       
+       </div>
+       
+       <div className="box-child-6-2">
+           <div className=" box-child-6-2-1">
+               {/* <i className="fa-regular fa-flag box-child-6-2-1-1"></i>  */}
+               <b className="box-child-6-2-1-2">10:00</b>
+               <p className="box-child-6-2-1-3">Ideatolifel</p>
+           </div>
+       
+           <div className="box-child-6-2-2">
+               <p className="box-child-6-2-2-1">10 m</p>
+                <p className="box-child-6-2-2-2"> (1 stop)</p>
+           </div>
+       
+           <div className="box-child-6-2-3">
+               {/* <i className="fa-solid fa-location-dot box-child-6-2-3-1"></i> */}
+               <b className="box-child-6-2-3-2">10:00</b>
+               <p className="box-child-6-2-3-3">Mochrou3 cafe</p>
+           </div>
+       </div>
+       
+           </div>
+       
+       <div className="box-child-6-3">
+           <button className="box-child-button-6-3-1">View Deatails</button>
+       </div>
+       
+       </div>
+    </div>
+
+
+    <div className="box-father-7">
+
+<div className="box-parent-7">
+
+<div className="box-child-7-1">
+
+<div className="box-child-7-1-1">
+ <div> 
+    <button className="box-button-child-7-1-1-1">
+    <div className="box-button-child-7-1-1-1-1">
+        <p>10</p>
+    </div>
+    <div>
+        <img src="dropdown.png" alt="" className="arrow-7"/>
+    </div>
+  </button> 
+ </div>
+
+  <div className="box-child-7-1-1-1-2">
+     <p>Showing 1 to 0 of 0 entries</p>
+
+  </div>
+
+</div>
+
+<div className="box-child-7-1-2">
+    <p>Trips Found:0</p>
+</div>
+
+<div className="box-child-7-1-3">
+    <img src="card.png" alt="" className="card-image-7"/>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+
+<div className="box-father-8">
+    
+    <div className="box-parent-8">
+
+        <div className="box-child-8">
+            <div className="box-child-8-1">
+
+            <div className="box-child-8-1-1">
+                <img src="Group.png" alt="" className="mobile-image-8"/>
+             </div>
+    
+                <div className="box-child-8-1-2">
+                    <div className="box-child-8-1-2-1">
+                   <p> Get Our App On The Store <b className="bold-8">GO</b> <b>!</b> </p>
+                    </div>
+
+                        <div className="box-child-8-1-2-2">
+                           <div> <img src="app store.png" alt="" className="image-app-8"/></div>
+                          <div>  <img src="google play.png" alt="" className="image-play-8"/> 
+                  </div>
+                </div>
+       </div>
+        </div>
+    </div>
+    </div>
+</div>
+
+
+  </div>
+
+  )
+}
+
+export default App
